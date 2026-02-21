bfce086a7543d7cd600c0bb8b2a61d51ed4f82e4
Brandon Pellumbi <brandonpellumbi@gmail.com>
2026-02-20 20:51:16 -0500

    Update orbit.py

--- src/orbit.py
+++ src/orbit.py
@@ -1,1 +1,9 @@
+EARTH_RADIUS_KM = 6371
 
+class Orbit:
+    def __init__(self, name, altitude_km):
+        self.name = name
+        self.altitude_km = altitude_km
+
+    def distance_from_earth_center(self):
+        return EARTH_RADIUS_KM + self.altitude_km
